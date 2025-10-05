3e74c427d7103f38d06249586b36ea10edb9d3a1
nallabellisriparthuu <nallabellisriparthu@gmail.com>
2025-10-05 06:10:54 +0530

    write about react

---
+++ 03Reuseability/src/App.jsx
@@ -0,0 +1,15 @@
+import Hello from "./Components/Hello";
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
